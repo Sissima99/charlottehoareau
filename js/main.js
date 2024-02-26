9adccf875693f12caab0706da542c5b9f1817200
Sissima99 <imasiss99@gmail.com>
2024-02-26 17:05:34 +0100

    v0

---
+++ js/main.js
@@ -0,0 +1,442 @@
+'use strict'; 
+$(window).load( function() {	
+    
+
+        // LIGHTBOX VIDEO
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+        
+//PRELOADER
+ $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+
+    
+if ($('.isotope_items').length) {
+
+    // PORTFOLIO ISOTOPE
+     var $container = $('.isotope_items');
+     $container.isotope();
+
+    $('.portfolio_filter ul li').on("click", function(){
+        $(".portfolio_filter ul li").removeClass("select-cat");
+        $(this).addClass("select-cat");				 
+        var selector = $(this).attr('data-filter');
+        $(".isotope_items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+    });
+        return false;
+    });  
+    
+}
+    
+}); // window load end 
+
+
+
+$(document).ready( function() {	
+    
+    
+    // WOW JS
+    new WOW({ mobile: false }).init();
+    
+    
+      
+    //SMOOTH SCROLL
+    $(document).on("scroll", onScroll);
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+             if ($(window).width() < 768) {
+                 $('.nav-menu').slideUp();
+             }
+        });
+            
+        $(this).addClass('active');
+      
+        var target = this.hash,
+        //menu = target;
+        target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': target.offset().top-85
+        }, 500, 'swing', function () {
+            window.location.hash = target.selector;
+            $(document).on("scroll", onScroll);
+        });
+    });
+    
+        
+        function onScroll(event){
+          if ($('#home').length) {     
+    var scrollPos = $(document).scrollTop();
+    $('nav ul li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top-90 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+   }              
+}
+    
+    
+    //NAVBAR SHOW - HIDE
+    $(window).scroll(function() {				
+    var scroll = $(window).scrollTop();
+    var homeheight = $(".home").height() -86;			
+
+    if (scroll > homeheight ) {												
+        $("nav").slideDown(100);
+        } else {
+        $("nav").slideUp(100);
+        }
+     }); 
+    
+    	
+ // RESPONSIVE MENU
+$('.responsive').on('click', function (e) {
+        $('.nav-menu').slideToggle();
+    });
+    
+    
+    // HOME PAGE HEIGHT
+     function centerInit() {
+        var hometext = $('.home')
+
+        hometext.css({
+            "height": $(window).height() + "px"
+        });
+    }
+    centerInit();
+    $(window).resize(centerInit);
+     
+    
+    
+    // HOME TYPED JS
+      $(".element").typed({
+        strings: ["Charlotte Hoareau", "a freelance writer", "a content strategist", "a digital nomad"],
+        typeSpeed: 15,
+        loop:true,
+        backDelay: 2000
+      });
+ 
+    
+    
+    // MAGNIFIC POPUP FOR PORTFOLIO PAGE
+    $('.link').magnificPopup({
+        type:'image',
+        gallery:{enabled:true},
+        zoom:{enabled: true, duration: 300}
+    });
+    
+       // OWL CAROUSEL GENERAL JS
+    var owlcar = $('.owl-carousel');
+    if (owlcar.length) {
+        owlcar.each(function () {
+            var $owl = $(this);
+            var itemsData = $owl.data('items');
+            var autoPlayData = $owl.data('autoplay');
+            var paginationData = $owl.data('pagination');
+            var navigationData = $owl.data('navigation');
+            var stopOnHoverData = $owl.data('stop-on-hover');
+            var itemsDesktopData = $owl.data('items-desktop');
+            var itemsDesktopSmallData = $owl.data('items-desktop-small');
+            var itemsTabletData = $owl.data('items-tablet');
+            var itemsTabletSmallData = $owl.data('items-tablet-small');
+            $owl.owlCarousel({
+                items: itemsData
+                , pagination: paginationData
+                , navigation: navigationData
+                , autoPlay: autoPlayData
+                , stopOnHover: stopOnHoverData
+                , navigationText: ["<", ">"]
+                , itemsCustom: [
+                    [0, 1]
+                    , [500, itemsTabletSmallData]
+                    , [710, itemsTabletData]
+                    , [992, itemsDesktopSmallData]
+                    , [1199, itemsDesktopData]
+                ]
+            , });
+        });
+    }
+    
+    
+}); // document ready end 
+
+
+
+/* Contact Form JS*/
+(function($){
+   'use strict'; 
+   
+   $(".contact-form").on('submit', function(e){
+        e.preventDefault();
+        
+        var uri = $(this).attr('action');
+        $("#con_submit").val('Wait...');
+        var con_name = $("#con_name").val();
+        var con_email = $("#con_email").val();
+        var con_message = $("#con_message").val();
+        
+        var required = 0;
+        $(".requie", this).each(function() {
+            if ($(this).val() == '')
+            {
+                $(this).addClass('reqError');
+                required += 1;
+            }
+            else
+            {
+                if ($(this).hasClass('reqError'))
+                {
+                    $(this).removeClass('reqError');
+                    if (required > 0)
+                    {
+                        required -= 1;
+                    }
+                }
+            }
+        });
+        if (required === 0)
+        {
+            $.ajax({
+                type: "POST",
+                url: 'mail.php',
+                data: {con_name: con_name, con_email: con_email, con_message: con_message},
+                success: function(data)
+                {
+                    $(".contact-form input, .contact-form textarea").val('');
+                    $("#con_submit, .sitebtn").val('Done!');
+					$("#con_submit .sitebtn").addClass("ok");
+                }
+            });
+        }
+        else
+        {
+            $("#con_submit, .sitebtn").val('Failed!');
+        }
+   });
+   $(".requie").keyup(function() {
+        $(this).removeClass('reqError');
+    });
+   
+})(jQuery);
+
+// Music player
+
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
+let track_name = document.querySelector('.track-name');
+let track_artist = document.querySelector('.track-artist');
+
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
+
+let seek_slider = document.querySelector('.seek_slider');
+let volume_slider = document.querySelector('.volume_slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let wave = document.getElementById('wave');
+let randomIcon = document.querySelector('.fa-random');
+let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+    {
+        img : 'images/cover/danse.jpg',
+        name : 'Une dernière danse',
+        artist : 'Kyo',
+        music : 'music/Une Dernière Danse.wav'
+    },
+    {
+        img : 'images/cover/lili.jpg',
+        name : 'Lili',
+        artist : 'Aaron',
+        music : 'music/Lili.wav'
+    },
+    {
+        img : 'images/cover/LOVE.jpg',
+        name : 'L.O.V.E',
+        artist : 'Nat King Cole',
+        music : 'music/L.O.V.E.wav' 
+    },
+    {
+        img : 'images/cover/beautiful.jpg',
+        name : 'You’re Beautiful',
+        artist : 'James Blunt',
+        music : 'music/Beautiful.wav'
+        
+    },
+    {
+        img : 'images/cover/weezer.jpg',
+        name : 'Island in the Sun',
+        artist : 'Weezer',
+        music : 'music/Island in the Sun.wav'
+        
+    },
+    {
+        img : 'images/cover/titan.jpg',
+        name : 'Instrumental',
+        artist : 'Titanic Theme Song',
+        music : 'music/Titanic.wav'
+        
+    },
+    {
+        img : 'images/cover/jesus.jpg',
+        name : 'Jesus in L.A.',
+        artist : 'Alec Benjamin (instrumental)',
+        music : 'music/Jesus in LA.wav'
+        
+    },
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
+}
+
+function random_bg_color(){
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
+    let a;
+
+    function populate(a){
+        for(let i=0; i<6; i++){
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x];
+            a += y;
+        }
+        return a;
+    }
+   
+}
+function reset(){
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+}
+function randomTrack(){
+    isRandom ? pauseRandom() : playRandom();
+}
+function playRandom(){
+    isRandom = true;
+    randomIcon.classList.add('randomActive');
+}
+function pauseRandom(){
+    isRandom = false;
+    randomIcon.classList.remove('randomActive');
+}
+function repeatTrack(){
+    let current_index = track_index;
+    loadTrack(current_index);
+    playTrack();
+}
+function playpauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
+}
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    track_art.classList.add('rotate');
+    wave.classList.add('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    track_art.classList.remove('rotate');
+    wave.classList.remove('loader');
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+function nextTrack(){
+    if(track_index < music_list.length - 1 && isRandom === false){
+        track_index += 1;
+    }else if(track_index < music_list.length - 1 && isRandom === true){
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    }else{
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }else{
+        track_index = music_list.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+function seekTo(){
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+function setVolume(){
+    curr_track.volume = volume_slider.value / 100;
+}
+function setUpdate(){
+    let seekPosition = 0;
+    if(!isNaN(curr_track.duration)){
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
+
+lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+    disableScrolling: true,
+  });
+  
